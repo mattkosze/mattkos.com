f7921118bdcc3c816202c6f8fdb1a46045df3068
mattkosze <matthew.koszewicz@gmail.com>
2025-07-06 23:06:38 -0400

    Rearranged files for deployment

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,3 @@
-import Headshot from '../assets/HeadshotTrimmed.png' 
-
 function Hero() {
     return (
         <div className="text-black flex flex-col-reverse sm:flex-row items-center justify-between px-14 py-40 gap-4 sm:gap-6 md:gap-10 lg:gap-20">
@@ -7,7 +5,7 @@ function Hero() {
                 <h1 className="font-semibold md:font-normal text-3xl sm:text-4xl md:text-6xl lg:text-[5rem]">MATT KOSZEWICZ</h1>
                 <h2 className="md:max-w-[80%] font-light sm:text-base md:text-base lg:text-xl">I love problem solving and have worked on startups (see <a href="" className="underline">Score</a>, <a href="" className="underline">Telemate</a>) all throughout college so far. Presently a rising junior studying CS + Psych at BU.</h2>
             </div>
-            <img src={Headshot} alt="My headshot" className="rounded-full w-40 sm:w-56 md:w-60 lg:w-80 h-auto"/>
+            <img src='public/HeadshotTrimmed.png'  alt="My headshot" className="rounded-full w-40 sm:w-56 md:w-60 lg:w-80 h-auto"/>
         </div>
     )
 }
